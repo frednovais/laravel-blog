433d8fcd418382c172545dd340f7bd2dc1631dde
Fabio Fonseca <flfonseca@gmail.com>
2019-10-07 16:15:50 +0100

    Descrição

--- resources/views/commom/links.blade.php
+++ resources/views/commom/links.blade.php
@@ -1,9 +1,8 @@
 <div class="links">
     <ul>
         <li><a href="/model-env">Model - Busacando o conteudo da variavel no Env</a></li>
-        <li><a href="/post-create">Model - Novo Post</a></li>
-        <li><a href="/post-index">Model - Lista de Posts</a></li>
-        
+        <li><a href="/post-create">Model - Novo Post (Insert)</a></li>
+        <li><a href="/post-index">Model - Lista de Posts (Show, Update, Edit e Delete)</a></li>
     </ul>
 </div>
 
